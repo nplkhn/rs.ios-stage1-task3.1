2d2003b4aa3375fcade8917d4e62f2f974d72d31
Nikita Plakhin <nplkhn1@yandex.ru>
2020-04-03 10:48:26 +0300

    feat: implement task1 - Polynomial

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,31 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    if ([numbers isEqualToArray:@[]]) {
+        return nil;
+    }
+    NSMutableString *resultString = [[NSMutableString alloc] init];
+    [numbers enumerateObjectsUsingBlock:^(NSNumber * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj.intValue > 1 && [resultString isEqualToString:@""]) {
+            [resultString appendFormat:@"%@", obj];
+        } else if (obj.intValue < 1 && [resultString isEqualToString:@""]) {
+            [resultString appendFormat:@"- %d", abs(obj.intValue)];
+        } else if (obj.intValue > 1) {
+            [resultString appendFormat:@" + %@", obj];
+        } else if (obj.intValue < -1) {
+            [resultString appendFormat:@" - %d", abs(obj.intValue)];
+        } else if (obj.intValue == 1) {
+            [resultString appendString:@" + "];
+        } else if (obj.intValue == -1) {
+            [resultString appendString:@" - "];
+        }
+        
+        if (obj.intValue && numbers.count - idx - 1 > 1) {
+            [resultString appendFormat:@"x^%lu",numbers.count - idx - 1];
+        } else if (obj.intValue && numbers.count - idx - 1 == 1) {
+            [resultString appendString:@"x"];
+        }
+    }];
+    return resultString;
 }
 @end
