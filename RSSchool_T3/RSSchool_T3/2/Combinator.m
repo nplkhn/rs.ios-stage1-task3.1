fc54123f6441664a53802ed94c0b252cd559881d
Nikita Plakhin <nplkhn1@yandex.ru>
2020-04-03 10:51:28 +0300

    feat: implement task2 - Combinator

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -1,7 +1,23 @@
 #import "Combinator.h"
 
+double factorialWithNumber(double number) {
+    double factorial = 1;
+    for (int i = 2; i <= number; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    int m = array[0].doubleValue, n = array[1].doubleValue, k = 1.0;
+    int C = factorialWithNumber(n)/(factorialWithNumber(k)*(factorialWithNumber(n-k)));
+    for (k = 1; k < n; k++) {
+        C = factorialWithNumber(n)/(factorialWithNumber(k)*(factorialWithNumber(n-k)));
+        if (C >= m) {
+            return [NSNumber numberWithDouble: k];
+        }
+    }
+    return nil;
 }
 @end
